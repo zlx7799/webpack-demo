fb51626558f68cd2def7caf4080fc133467459f3
zhoulx <zhoulx917@foxmail.com>
2022-11-28 13:55:11 +0800

    加载css

--- src/index.js
+++ src/index.js
@@ -3,13 +3,15 @@
  * @Author: zhoulx
  * @Date: 2022-11-25 14:25:03
  * @LastEditors: zhoulx
- * @LastEditTime: 2022-11-25 15:18:09
+ * @LastEditTime: 2022-11-28 13:51:51
  */
 import { join } from 'lodash';
+import './style.css';
 function component () {
   const element = document.createElement('div');
   
   element.innerHTML = join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
   return element;
 }
 
