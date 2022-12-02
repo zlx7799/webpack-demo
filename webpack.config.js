cd33f75a7386dd952488e92e60b119d0496d1768
zhoulx <zhoulx917@foxmail.com>
2022-12-02 16:14:09 +0800

    加载字体

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,11 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)/i,
-        type: 'asset/resource'
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)/i,
+        type: 'asset/resource',
       }
     ]
   }
